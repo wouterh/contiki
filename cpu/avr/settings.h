33b4ba5f8b2c07d8e177c350392103d6cf031acc
Robert Quattlebaum <darco@deepdarc.com>
2011-03-25 15:30:03 -0700

    Added some more documentation. Added functions for traversing all of the settings.

--- cpu/avr/settings.h
+++ cpu/avr/settings.h
@@ -4,8 +4,26 @@
 #include <inttypes.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "dev/eeprom.h"
+
+#pragma mark - Types
+
+typedef enum {
+	SETTINGS_STATUS_OK=0,
+	SETTINGS_STATUS_INVALID_ARGUMENT,
+	SETTINGS_STATUS_NOT_FOUND,
+	SETTINGS_STATUS_OUT_OF_SPACE,
+	SETTINGS_STATUS_UNIMPLEMENTED,
+	SETTINGS_STATUS_FAILURE,
+} settings_status_t;
+
+typedef eeprom_addr_t settings_iter_t;
 
 typedef uint16_t settings_key_t;
+
+typedef uint16_t settings_length_t;
+
+#pragma mark - Settings Keys
 
 #define SETTINGS_KEY_EUI64			'E8'	//!< Value always 8 bytes long
 #define SETTINGS_KEY_EUI48			'E6'	//!< Value always 8 bytes long
@@ -17,37 +35,56 @@ typedef uint16_t settings_key_t;
 #define SETTINGS_KEY_AES128ENABLED	'SE'	//!< Value always 16 bytes long
 #define SETTINGS_KEY_HOSTNAME		'HN'	//!< Variable Length
 #define SETTINGS_KEY_DOMAINNAME		'DN'	//!< Variable Length
+
+#pragma mark - Experimental Settings Keys
+
 #define SETTINGS_KEY_RDC_INDEX		'RD'	//!< Always 1 byte long
 
-typedef enum {
-	SETTINGS_STATUS_OK=0,
-	SETTINGS_STATUS_INVALID_ARGUMENT,
-	SETTINGS_STATUS_NOT_FOUND,
-	SETTINGS_STATUS_OUT_OF_SPACE,
-	SETTINGS_STATUS_UNIMPLEMENTED,
-	SETTINGS_STATUS_FAILURE,
-} settings_status_t;
+#pragma mark - Constants
 
 // Use this when you want to retrieve the last item
 #define SETTINGS_LAST_INDEX		(0xFF)
 
-#define SETTINGS_INVALID_KEY	(0x00)
+#define SETTINGS_INVALID_KEY	(0x0000)
+
+#define SETTINGS_INVALID_ITER	(EEPROM_NULL)
 
 #define SETTINGS_MAX_VALUE_SIZE	(0x3FFF)	// 16383 bytes
 
-extern settings_status_t settings_get(settings_key_t key,uint8_t index,unsigned char* value,size_t* value_size);
-extern settings_status_t settings_add(settings_key_t key,const unsigned char* value,size_t value_size);
+#pragma mark - Settings accessors
+
+extern settings_status_t settings_get(settings_key_t key,uint8_t index,unsigned char* value,settings_length_t* value_size);
+extern settings_status_t settings_add(settings_key_t key,const unsigned char* value,settings_length_t value_size);
 extern bool settings_check(settings_key_t key,uint8_t index);
 extern void settings_wipe(void);
 
-extern settings_status_t settings_set(settings_key_t key,const unsigned char* value,size_t value_size);
+extern settings_status_t settings_set(settings_key_t key,const unsigned char* value,settings_length_t value_size);
 extern settings_status_t settings_delete(settings_key_t key,uint8_t index);
 
-//#pragma mark -
-//#pragma mark Inline convenience functions
+#pragma mark - Settings traversal functions
+
+/*!	Will return extern SETTINGS_INVALID_ITER if at the end of settings list */
+extern settings_iter_t settings_iter_begin();
+
+/*!	Will return extern SETTINGS_INVALID_ITER if at the end of settings list */
+extern settings_iter_t settings_iter_next(settings_iter_t item_addr);
+
+extern bool settings_iter_is_valid(settings_iter_t item_addr);
+
+extern settings_key_t settings_iter_get_key(settings_iter_t item_addr);
+
+extern settings_length_t settings_iter_get_value_length(settings_iter_t item_addr);
+
+extern eeprom_addr_t settings_iter_get_value_addr(settings_iter_t item_addr);
+
+// Not yet implemented.
+//extern void settings_iter_get_value_bytes(settings_iter_t item_addr, void* bytes);
+//extern void settings_iter_delete(settings_iter_t item);
+
+#pragma mark - Inline convenience functions
 
 static inline const char*
-settings_get_cstr(settings_key_t key,uint8_t index,char* c_str,size_t c_str_size) {
+settings_get_cstr(settings_key_t key,uint8_t index,char* c_str,settings_length_t c_str_size) {
 	c_str_size--;
 	if(settings_get(key,index,(unsigned char*)c_str,&c_str_size)==SETTINGS_STATUS_OK) {
 		// Zero terminate.
@@ -61,7 +98,7 @@ settings_get_cstr(settings_key_t key,uint8_t index,char* c_str,size_t c_str_size
 static inline bool
 settings_get_bool_with_default(settings_key_t key,uint8_t index, bool default_value) {
 	uint8_t ret = default_value;
-	size_t sizeof_uint8 = sizeof(uint8_t);
+	settings_length_t sizeof_uint8 = sizeof(uint8_t);
 	settings_get(key,index,(unsigned char*)&ret,&sizeof_uint8);
 	return (bool)ret;
 }
@@ -69,7 +106,7 @@ settings_get_bool_with_default(settings_key_t key,uint8_t index, bool default_va
 static inline uint8_t
 settings_get_uint8(settings_key_t key,uint8_t index) {
 	uint8_t ret = 0;
-	size_t sizeof_uint8 = sizeof(uint8_t);
+	settings_length_t sizeof_uint8 = sizeof(uint8_t);
 	settings_get(key,index,(unsigned char*)&ret,&sizeof_uint8);
 	return ret;
 }
@@ -87,7 +124,7 @@ settings_set_uint8(settings_key_t key,uint8_t value) {
 static inline uint16_t
 settings_get_uint16(settings_key_t key,uint8_t index) {
 	uint16_t ret = 0;
-	size_t sizeof_uint16 = sizeof(uint16_t);
+	settings_length_t sizeof_uint16 = sizeof(uint16_t);
 	settings_get(key,index,(unsigned char*)&ret,&sizeof_uint16);
 	return ret;
 }
@@ -105,7 +142,7 @@ settings_set_uint16(settings_key_t key,uint16_t value) {
 static inline uint32_t
 settings_get_uint32(settings_key_t key,uint8_t index) {
 	uint32_t ret = 0;
-	size_t sizeof_uint32 = sizeof(uint32_t);
+	settings_length_t sizeof_uint32 = sizeof(uint32_t);
 	settings_get(key,index,(unsigned char*)&ret,&sizeof_uint32);
 	return ret;
 }
@@ -123,7 +160,7 @@ settings_set_uint32(settings_key_t key,uint32_t value) {
 static inline uint64_t
 settings_get_uint64(settings_key_t key,uint8_t index) {
 	uint64_t ret = 0;
-	size_t sizeof_uint64 = sizeof(uint64_t);
+	settings_length_t sizeof_uint64 = sizeof(uint64_t);
 	settings_get(key,index,(unsigned char*)&ret,&sizeof_uint64);
 	return ret;
 }
